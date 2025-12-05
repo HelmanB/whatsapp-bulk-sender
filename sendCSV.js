0b105bf9d61cbb06407a7f9838588ee22c978f8b
HelmanB <100032900+HelmanB@users.noreply.github.com>
2025-12-05 16:48:55 -0500

    Change language code from 'en_GB' to 'es_CO'

--- sendCSV.js
+++ sendCSV.js
@@ -16,7 +16,7 @@ async function enviarTemplate(numero, nombre) {
       type: "template",
       template: {
         name: "saludo_prueba",   // NOMBRE EXACTO DE LA PLANTILLA APROBADA
-        language: { code: "en_GB" },
+        language: { code: "es_CO" },
         components: [
           {
             type: "body",
